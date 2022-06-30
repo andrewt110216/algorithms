4e336db8e301e4aeb7df7ef8cb3e67b2212baf61
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-06-30 09:53:55 -0700

    add new solution to LC 283

--- leetcode/p283_move_zeroes.py
+++ leetcode/p283_move_zeroes.py
@@ -23,11 +23,20 @@
 # Follow up:
 # Could you minimize the total number of operations done?
 
-debug = False  # DO NOT CHANGE HERE. False for file import. Change below.
-
 # ================================= SOLUTION =================================
 
 def solution(nums):
+    """V3 created 6.30.22. Simpler two-pointer solution."""
+    next_int = 0
+    for i, num in enumerate(nums):
+        if num != 0:
+            # swap elements i and next_int, if necessary
+            if i != next_int:
+                nums[i], nums[next_int] = nums[next_int], nums[i]
+            next_int += 1
+    return nums
+
+def solution2(nums):
     """V2 appropriately uses two actual pointers. See below for V1."""
 
     # If array is of length 1 or less, no action needed
@@ -69,120 +78,84 @@ def solution1(nums):
 
     return nums
 
-# ================================ TEST CASES ================================
+# =============================== DRIVER CODE ================================
 
 if __name__ == '__main__':
-
-# ----------------------------------------------------------------------------
-# Set to true to see debugging print statements, if any, from execution
-    debug = False
-# ----------------------------------------------------------------------------
+    from datetime import datetime
 
     tests = 0
     failed_tests = 0
     divider_width = 78
 
-# New Test Case --------------------------------------------------------------
+# Decorator Function for Testing Output --------------------------------------
 
-    print(f" TEST CASE: Example 1 ".center(divider_width, "-"))
-    tests += 1
-    test_input = [0,1,0,3,12]
-    print('Input:', test_input)
-    result = solution(test_input)
-    print('Output:', result)
+    def test_decorator(func):
+        """Decorator function to wrap around the solution"""
+        def wrapper(expected, description, *args, **kwargs):
+            global tests, failed_tests, divider_width
+            tests += 1
+            print(f" TEST CASE: {description} ".center(divider_width, "-"))
+            print('Input:', *args, **kwargs)
+            start = datetime.now()
+            result = func(*args, **kwargs)
+            print('Output:', result)
+            print('Time:', datetime.now() - start)
 
+            if result == expected:
+                print("\n > Test Result: **PASS!**\n")
+            else:
+                print("\n > Test Result: **FAIL.**\n")
+                print(f"\t > Expected Result: {expected_result}\n")
+                failed_tests += 1
+
+        return wrapper
+
+    # For testing, decorate the solution function
+    solution = test_decorator(solution)
+
+# ADD TEST CASES HERE--------------------------------------------------------
+
+    # Test Case Block
+    test_case_description = 'Example 1'
+    args = [[0,1,0,3,12]]
+    kwargs = {}
     expected_result = [1,3,12,0,0]
-    if result == expected_result:
-        print("\n > Test Result: **PASS!**\n")
-    else:
-        print("\n > Test Result: **FAIL.**\n")
-        print(f"\t > Expected Result: {expected_result}\n")
-        failed_tests += 1
+    solution(expected_result, test_case_description, *args, **kwargs)
 
-# New Test Case --------------------------------------------------------------
-
-    print(f" TEST CASE: Example 2 ".center(divider_width, "-"))
-    tests += 1
-    test_input = [0]
-    print('Input:', test_input)
-    result = solution(test_input)
-    print('Output:', result)
-
+    # Test Case Block
+    test_case_description = 'Example 2'
+    args = [[0]]
+    kwargs = {}
     expected_result = [0]
-    if result == expected_result:
-        print("\n > Test Result: **PASS!**\n")
-    else:
-        print("\n > Test Result: **FAIL.**\n")
-        print(f"\t > Expected Result: {expected_result}\n")
-        failed_tests += 1
+    solution(expected_result, test_case_description, *args, **kwargs)
 
-# New Test Case --------------------------------------------------------------
+    # Test Case Block
+    test_case_description = 'No Zeroes'
+    args = [[1,2,3,4]]
+    kwargs = {}
+    expected_result = [1,2,3,4]
+    solution(expected_result, test_case_description, *args, **kwargs)
 
-    print(f" TEST CASE: No Zeroes ".center(divider_width, "-"))
-    tests += 1
-    test_input = [1, 2, 3, 4]
-    print('Input:', test_input)
-    result = solution(test_input)
-    print('Output:', result)
+    # Test Case Block
+    test_case_description = 'All Zeroes'
+    args = [[0,0,0,0]]
+    kwargs = {}
+    expected_result = [0,0,0,0]
+    solution(expected_result, test_case_description, *args, **kwargs)
 
-    expected_result = [1, 2, 3, 4]
-    if result == expected_result:
-        print("\n > Test Result: **PASS!**\n")
-    else:
-        print("\n > Test Result: **FAIL.**\n")
-        print(f"\t > Expected Result: {expected_result}\n")
-        failed_tests += 1
+    # Test Case Block
+    test_case_description = 'Many Zeroes'
+    args = [[0,0,0,0,1]]
+    kwargs = {}
+    expected_result = [1,0,0,0,0]
+    solution(expected_result, test_case_description, *args, **kwargs)
 
-# New Test Case --------------------------------------------------------------
-
-    print(f" TEST CASE: All Zeroes ".center(divider_width, "-"))
-    tests += 1
-    test_input = [0, 0, 0, 0, 0]
-    print('Input:', test_input)
-    result = solution(test_input)
-    print('Output:', result)
-
-    expected_result = [0, 0, 0, 0, 0]
-    if result == expected_result:
-        print("\n > Test Result: **PASS!**\n")
-    else:
-        print("\n > Test Result: **FAIL.**\n")
-        print(f"\t > Expected Result: {expected_result}\n")
-        failed_tests += 1
-
-# New Test Case --------------------------------------------------------------
-
-    print(f" TEST CASE: Many Zeroes ".center(divider_width, "-"))
-    tests += 1
-    test_input = [0, 0, 0, 0, 0, 1]
-    print('Input:', test_input)
-    result = solution(test_input)
-    print('Output:', result)
-
-    expected_result = [1, 0, 0, 0, 0, 0]
-    if result == expected_result:
-        print("\n > Test Result: **PASS!**\n")
-    else:
-        print("\n > Test Result: **FAIL.**\n")
-        print(f"\t > Expected Result: {expected_result}\n")
-        failed_tests += 1
-
-# New Test Case --------------------------------------------------------------
-
-    print(f" TEST CASE: Longer ".center(divider_width, "-"))
-    tests += 1
-    test_input = [0, 1, 2, 0, 4, 0, 5, 6, 7, 8, 9, 0, 10]
-    print('Input:', test_input)
-    result = solution(test_input)
-    print('Output:', result)
-
+    # Test Case Block
+    test_case_description = 'Longer, Complicated'
+    args = [[0, 1, 2, 0, 4, 0, 5, 6, 7, 8, 9, 0, 10]]
+    kwargs = {}
     expected_result = [1, 2, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0, 0]
-    if result == expected_result:
-        print("\n > Test Result: **PASS!**\n")
-    else:
-        print("\n > Test Result: **FAIL.**\n")
-        print(f"\t > Expected Result: {expected_result}\n")
-        failed_tests += 1
+    solution(expected_result, test_case_description, *args, **kwargs)
 
 # SUMMARY ====================================================================
 
