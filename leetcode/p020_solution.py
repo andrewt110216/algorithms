32a1298ec39beaf01a7ee8bb2eb018f6bdbb9381
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-07-20 13:12:29 -0700

    add 2nd implementation to LC p020

--- leetcode/p020_solution.py
+++ leetcode/p020_solution.py
@@ -7,9 +7,64 @@ from collections import deque
 class Solution:
 
     # list the methods to be run against the test cases
-    implementations = ['valid_parentheses']
+    implementations = ['valid_parentheses', 'valid_parentheses_first']
 
     def valid_parentheses(self, s: str) -> bool:
+        """
+        A stack is the proper data structure to confirm that open brackets are
+        closed correctly. Add open brackets to the stack and when a closed
+        bracket is encountered, pop from the stack and check that these two
+        brackets match
+
+        This implementation is simplified to be easier to read and validates
+        that each character is in "()[]{}"
+
+        Time: O(n) (iterate over s once)
+        Space: O(n) (max length of the stack, if no closing brackets)
+        """
+
+        # adjacency lists for valid and matching brackets
+        opening = ["(", "[", "{"]
+        closing = [")", "]", "}"]
+
+        stack = deque()  # store open brackets yet to be closed
+
+        # iterate over each character in s
+        # we are searching for any invalid brackets. if none found, s is valid
+        for char in s:
+
+            # if an open bracket, add it to the stack
+            if char in opening:
+                stack.append(char)
+
+            # if a closing bracket, validate it against top bracket from stack
+            elif char in closing:
+
+                # check that the index of the top element from stack in the
+                # opening brackets list matches the index of char
+                # in the closing brackets list 
+                if stack:
+                    open = stack.pop()
+                    if closing.index(char) != opening.index(open):
+                        return False
+
+                # if stack is empty, this closing bracket is invald
+                else:
+                    return False
+
+            # if char is not in opening or closing lists, it is invalid
+            else:
+                return False
+
+        # if stack is not empty, an open bracket was not closed so s is invalid
+        if stack:
+            return False
+
+        # nothing invalid was found, so s must be valid
+        return True
+
+
+    def valid_parentheses_first(self, s: str) -> bool:
         """
         A stack is the proper data structure to confirm that open brackets are
         closed correctly. Add open brackets to the stack and when a closed
