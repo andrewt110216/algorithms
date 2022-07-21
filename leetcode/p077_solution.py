58fd4d72ddbd799899efd92f79e5a8c598189164
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-07-21 15:42:51 -0700

    add tests for LC p077

---
+++ leetcode/p077_solution.py
@@ -0,0 +1,61 @@
+# 077 - Combinations
+# https://leetcode.com/problems/combinations/
+
+from itertools import combinations
+
+
+class Solution:
+
+    # list the methods to be run against the test cases
+    implementations = ['get_combinations']
+
+    def get_combinations(self, n: int, k: int) -> list[list[int]]:
+        """
+        Use backtracking
+
+        Time: O(n choose k) (each combination is generated exactly once)
+        Space: O(n choose k) (the size of the call stack)
+        """
+
+        def recurse(start, cur_combo=[]):
+
+            # check if the current combination is complete (i.e. has length k)
+            if len(cur_combo) == k:
+                combinations.append(cur_combo[:])  # use a copy of cur_combo
+                return  # prevent cur_combo to continue growing past length k
+
+            # iterate over the intergers from start to n
+            for i in range(start, n + 1):
+
+                # add i to the current combination
+                cur_combo.append(i)
+
+                # recurse  with i + 1 to complete the combination
+                recurse(i + 1, cur_combo=cur_combo)
+
+                # a current combination was completed and added to combinations
+                # so remove the first element and continue
+                cur_combo.pop()
+
+        combinations = []
+        recurse(1)
+        return combinations
+
+
+# =============================== DRIVER CODE ================================
+
+if __name__ == '__main__':
+
+    from class_print_tests import PrintTests as PT
+
+    # enter test cases: ['description', [inputs], expected_result]
+    test_cases = [
+        ['Example 1', [4, 2],
+            [[2, 4], [3, 4], [2, 3], [1, 2], [1, 3], [1, 4]]],
+        ['Example 2', [1, 1], [[1]]],
+        ['Large Output', [10, 3], list(combinations(range(1, 11), 3))],
+    ]
+
+    # run test cases and print results using PrintTests class
+    pt = PT(Solution(), test_cases)
+    pt.run()
---
+++ leetcode/p077_test.py
@@ -0,0 +1,43 @@
+from p077_solution import Solution
+from itertools import combinations
+
+
+class TestClass:
+
+    s = Solution()
+
+    def run_funcs(self, args, expected):
+        """Run test case for each implementation in Solution"""
+        for implementation in self.s.implementations:
+            func = getattr(self.s, implementation)
+            result = func(*args)
+
+            # if result is list, sort it and expected result for comparison
+            for var in [result, expected]:
+                if type(var) in [list, tuple]:
+                    var.sort()
+                    if type(var[0]) in [list, tuple]:
+                        [x.sort() for x in var]
+
+            assert result == expected
+
+    def test1_example1(self):
+        args = [4, 2]
+        expected = [[2, 4], [3, 4], [2, 3], [1, 2], [1, 3], [1, 4]]
+        self.run_funcs(args, expected)
+
+    def test2_example2(self):
+        args = [1, 1]
+        expected = [[1]]
+        self.run_funcs(args, expected)
+
+    def test3_large_output(self):
+        args = [10, 3]
+        expected = list(combinations(range(1, 11), 3))
+
+        # need to convert combinations from itertools.combinations
+        # from tuples to lists, for comparison against our function result
+        for i in range(len(expected)):
+            expected[i] = list(expected[i])
+
+        self.run_funcs(args, expected)
