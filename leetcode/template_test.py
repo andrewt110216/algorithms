359c72cb0c9b2d8d35e6521ad341ed6f4078f339
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-07-21 12:19:03 -0700

    add sorting of result and expected result for assertion

--- leetcode/template_test.py
+++ leetcode/template_test.py
@@ -10,7 +10,16 @@ class TestClass:
         """Run test case for each implementation in Solution"""
         for implementation in self.s.implementations:
             func = getattr(self.s, implementation)
-            assert func(*args) == expected
+            result = func(*args)
+
+            # if result is list, sort it and expected result for comparison
+            for var in [result, expected]:
+                if type(var) is list:
+                    var.sort()
+                    if type(var[0]) is list:
+                        [x.sort() for x in var]
+
+            assert result == expected
 
     def test1_example1(self):
         args = [True]
