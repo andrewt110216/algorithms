98135e8831f152606fd1131e6a2072a2f000643a
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-07-25 10:35:29 -0700

    remove sorting of result, not necessary

--- leetcode/p141_test.py
+++ leetcode/p141_test.py
@@ -11,14 +11,6 @@ class TestClass:
         for implementation in self.s.implementations:
             func = getattr(self.s, implementation)
             result = func(*args)
-
-            # if result is list, sort it and expected result for comparison
-            for var in [result, expected]:
-                if type(var) is list:
-                    var.sort()
-                    if type(var[0]) is list:
-                        [x.sort() for x in var]
-
             assert result == expected
 
     def test1_example1(self):
