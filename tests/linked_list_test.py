06a607459239feba44c0bab3279b12c0f567f9f8
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-08-20 13:37:58 -0700

    remove definition of default scope: function

---
+++ tests/linked_list_test.py
@@ -0,0 +1,13 @@
+import pytest
+from data_structures.linked_list import ListNode
+
+
+# Fixtures
+@pytest.fixture
+def new_linked_list():
+    return None
+
+
+# Tests
+def test_create_heap(new_heap):
+    assert 0
