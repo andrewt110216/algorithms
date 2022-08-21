b50ef30ed2134c07fdc9e5f1134e410e21f664a6
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-08-20 23:35:01 -0700

    add additional methods with tests to linked list ds. not finished yet.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -43,8 +43,8 @@ class LinkedList:
         """Compare self to another linked list for equality"""
         if type(list2) != LinkedList:
             return False
-        cur1 = self.get_head()
-        cur2 = list2.get_head()
+        cur1 = self.get_head(quiet=True)
+        cur2 = list2.get_head(quiet=True)
         while cur1 and cur2:
             if cur1.val != cur2.val:
                 return False
@@ -58,12 +58,16 @@ class LinkedList:
         """Return self._length == 0"""
         return self._length == 0
 
-    def get_head(self):
-        """Return head node"""
+    def get_head(self, quiet: bool = False):
+        """Return head node. Set quiet to True to return None if empty."""
+        if (not quiet) and self.is_empty():
+            raise IndexError("Cannot get head of empty LinkedList")
         return self._prehead.next
 
-    def get_tail(self):
-        """Return tail node"""
+    def get_tail(self, quiet: bool = False):
+        """Return tail node. Set quiet to True to return None if empty."""
+        if (not quiet) and self.is_empty():
+            raise IndexError("Cannot get tail of empty LinkedList")
         return self._tail
 
     def insert(self, index: int, value: int):
@@ -100,7 +104,7 @@ class LinkedList:
         """Return value of node at index"""
         if not 0 <= index < self._length:
             raise IndexError
-        cur = self.get_head()
+        cur = self.get_head(quiet=True)
         for _ in range(index):
             cur = cur.next
         return cur.val
@@ -108,7 +112,7 @@ class LinkedList:
     def find_value(self, target: int) -> int:
         """Return index of first occurence of target value in self"""
         cur_idx = 0
-        cur = self.get_head()
+        cur = self.get_head(quiet=True)
         while cur:
             if cur.val == target:
                 return cur_idx
@@ -119,42 +123,83 @@ class LinkedList:
     def count(self, target: int) -> int:
         """Count number of ocurrences of target value in self"""
         count = 0
-        cur = self.get_head()
+        cur = self.get_head(quiet=True)
         while cur:
             if cur.val == target:
                 count += 1
             cur = cur.next
         return count
 
-    # TODO
     def find_all(self, target: int) -> list:
         """Return indices of all occurences of target value in self"""
-        pass
+        indices = []
+        cur_idx = 0
+        cur = self._prehead.next
+        while cur:
+            if cur.val == target:
+                indices.append(cur_idx)
+            cur = cur.next
+            cur_idx += 1
+        return indices
 
     def pop(self) -> int:
         """Delete tail node and return its value"""
-        pass
+        if self.is_empty():
+            raise IndexError("Cannot pop from empty LinkedList")
+        prev = self._prehead
+        n = self._length
+        for _ in range(n - 1):
+            prev = prev.next
+        popped = prev.next.val
+        prev.next = None
+        self._tail = prev
+        self._length -= 1
+        return popped
 
     def pop_left(self) -> int:
         """Delete head node and return its value"""
-        pass
-
-    def push(self) -> None:
-        """Push a new node to end of list"""
-        pass
-
-    def push_left(self) -> None:
-        """Push a new node to front of list"""
-        pass
+        if self.is_empty():
+            raise IndexError("Cannot pop from empty LinkedList")
+        prev = self._prehead
+        popped = prev.next.val
+        prev.next = prev.next.next
+        self._length -= 1
+        return popped
 
     def to_array(self) -> list:
         """Return a traditional list representation of self"""
-        pass
+        out = []
+        cur = self.get_head(quiet=True)
+        while cur:
+            out.append(cur.val)
+            cur = cur.next
+        return out
+
+    def push(self, value: int) -> None:
+        """Push a new node to end of list"""
+        new_node = ListNode(value)
+        prev = self._prehead if self.is_empty() else self.get_tail(quiet=True)
+        prev.next = new_node
+        self._tail = new_node
+        self._length += 1
+
+    def push_left(self, value: int) -> None:
+        """Push a new node to front of list"""
+        new_node = ListNode(value)
+        prev = self._prehead
+        if self.is_empty():
+            new_node.next = None
+            self._tail = new_node
+        else:
+            new_node.next = prev.next
+        prev.next = new_node
+        self._length += 1
 
     def show(self) -> None:
         """Print items of self as a traditional list"""
         print(self.to_array())
 
+    # TODO
     def delete(self, target: int) -> None:
         """Delete first occurence of target value from self"""
         pass
--- tests/linked_list_test.py
+++ tests/linked_list_test.py
@@ -48,9 +48,16 @@ class TestLinkedList:
         return [5, 8, 3, 4, 9, 10, 2, 1, 7, 6]
 
     @pytest.fixture
-    def list_ten(self, new_list, array_ten):
-        new_list.from_array(array_ten)
-        return new_list
+    def array_twos(self):
+        return [2, 2, 2, 2]
+
+    @pytest.fixture
+    def list_ten(self, array_ten):
+        return LinkedList(array_ten)
+
+    @pytest.fixture
+    def list_twos(self, array_twos):
+        return LinkedList(array_twos)
 
     # Tests
 
@@ -116,10 +123,10 @@ class TestLinkedList:
         new_list.from_array(array_ten)
         assert len(new_list) == 10
         head = new_list.get_head()
-        assert head.val == 5
-        assert head.next.next.val == 3
-        assert head.next.next.next.next.val == 9
-        assert new_list.get_tail().val == 6
+        assert head.val == array_ten[0]
+        assert head.next.next.val == array_ten[2]
+        assert head.next.next.next.next.val == array_ten[4]
+        assert new_list.get_tail().val == array_ten[-1]
 
     def test_create_new_list_with_array(self, array_ten, list_ten):
         my_list = LinkedList(array_ten)
@@ -128,20 +135,20 @@ class TestLinkedList:
 
     # Get value by position
     def test_get_head_none(self, new_list):
-        assert len(new_list) == 0
-        assert new_list.get_head() is None
+        with pytest.raises(IndexError, match='not get head'):
+            new_list.get_head()
 
-    def test_get_head_ten(self, list_ten):
+    def test_get_head_ten(self, list_ten, array_ten):
         assert len(list_ten) == 10
-        assert list_ten.get_head().val == 5
+        assert list_ten.get_head().val == array_ten[0]
 
     def test_get_tail_none(self, new_list):
-        assert len(new_list) == 0
-        assert new_list.get_tail() is None
+        with pytest.raises(IndexError, match='not get tail'):
+            new_list.get_tail()
 
-    def test_get_tail_ten(self, list_ten):
+    def test_get_tail_ten(self, list_ten, array_ten):
         assert len(list_ten) == 10
-        assert list_ten.get_tail().val == 6
+        assert list_ten.get_tail().val == array_ten[-1]
 
     def test_get_value(self, list_ten, array_ten):
         for idx, num in enumerate(array_ten):
@@ -166,6 +173,23 @@ class TestLinkedList:
         with pytest.raises(ValueError):
             new_list.find_value(1)
 
+    def test_find_all_empty(self, new_list):
+        assert new_list.find_all(1) == []
+
+    def test_find_all_one(self, list_ten, array_ten):
+        assert list_ten.find_all(5) == [array_ten.index(5)]
+        assert list_ten.find_all(9) == [array_ten.index(9)]
+        assert list_ten.find_all(6) == [array_ten.index(6)]
+
+    def test_find_all_three(self):
+        my_list = LinkedList([0, 5, 0, 5, 5, 0])
+        assert my_list.find_all(0) == [0, 2, 5]
+        assert my_list.find_all(5) == [1, 3, 4]
+
+    def test_find_all_every(self, array_twos):
+        my_list = LinkedList(array_twos)
+        assert my_list.find_all(2) == [0, 1, 2, 3]
+
     # Count
     def test_count_empty(self, new_list):
         for target in [-1, 0, 1, 10]:
@@ -179,8 +203,8 @@ class TestLinkedList:
         for target in array_ten:
             assert list_ten.count(target) == 1
 
-    def test_count_multiple(self):
-        my_list = LinkedList([2, 2, 2, 2])
+    def test_count_multiple(self, array_twos):
+        my_list = LinkedList(array_twos)
         assert my_list.count(2) == 4
 
     # Equality
@@ -198,14 +222,91 @@ class TestLinkedList:
         list2 = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         assert list_ten != list2
 
+    # Pop
+    def test_pop_error(self, new_list):
+        with pytest.raises(IndexError, match="not pop from empty"):
+            new_list.pop()
+
+    def test_pop_left_error(self, new_list):
+        with pytest.raises(IndexError, match="not pop from empty"):
+            new_list.pop_left()
+
+    def test_pop_ten(self, list_ten, array_ten):
+        for i in range(10):
+            assert list_ten.pop() == array_ten[9 - i]
+            assert len(list_ten) == 9 - i
+        assert list_ten.is_empty()
+
+    def test_pop_left_ten(self, list_ten, array_ten):
+        for i in range(10):
+            assert list_ten.pop_left() == array_ten[i]
+            assert len(list_ten) == 9 - i
+        assert list_ten.is_empty()
+
+    def test_pop_one(self):
+        my_list = LinkedList([1])
+        assert len(my_list) == 1
+        assert my_list.pop() == 1
+        assert my_list.is_empty()
+
+    def test_pop_left_one(self):
+        my_list = LinkedList([1])
+        assert len(my_list) == 1
+        assert my_list.pop_left() == 1
+        assert my_list.is_empty()
+
+    # To array
+    def test_to_array_empty(self, new_list, capsys):
+        assert new_list.to_array() == []
+        new_list.show()
+        assert capsys.readouterr().out == "[]\n"
+
+    def test_to_array_ten(self, list_ten, array_ten, capsys):
+        assert list_ten.to_array() == array_ten
+        list_ten.show()
+        assert capsys.readouterr().out == str(array_ten) + "\n"
+
+    def test_to_array_twos(self, list_twos, array_twos, capsys):
+        assert list_twos.to_array() == array_twos
+        list_twos.show()
+        assert capsys.readouterr().out == str(array_twos) + "\n"
+
+    def test_to_array_one(self, capsys):
+        my_list = LinkedList([4])
+        assert my_list.to_array() == [4]
+        my_list.show()
+        assert capsys.readouterr().out == "[4]\n"
+
+    # Push
+    def test_push_empty(self, new_list):
+        new_list.push(1)
+        new_list.push(2)
+        new_list.push(3)
+        assert len(new_list) == 3
+        assert new_list.get_head().val == 1
+        assert new_list.get_tail().val == 3
+        assert new_list.to_array() == [1, 2, 3]
+
+    def test_push_left_empty(self, new_list):
+        new_list.push_left(1)
+        new_list.push_left(2)
+        new_list.push_left(3)
+        assert len(new_list) == 3
+        assert new_list.get_head().val == 3
+        assert new_list.get_tail().val == 1
+        assert new_list.to_array() == [3, 2, 1]
+
+    def test_push_ten(self, list_ten):
+        list_ten.push(15)
+        assert list_ten.get_tail().val == 15
+        assert len(list_ten) == 11
+
+    def test_push_left_ten(self, list_ten):
+        list_ten.push_left(-15)
+        assert list_ten.get_head().val == -15
+        assert len(list_ten) == 11
+
     # TODO
-    # - find all
-    # - pop
-    # - pop_left
-    # - push
-    # - push_left
-    # - to_array
-    # - show
     # - delete
     # - delete_index
     # - copy
